629cc9f5b0c133dfc6323ee86e61e8114ba24cfb
Nansess <75940580+Nansess@users.noreply.github.com>
2023-11-18 16:39:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ const cheerio = require('cheerio');
 
 async function getArtworkUrl(appleMusicLink) {
   try {
-    const response = await axios.get(appleMusicLink);
-    const $ = cheerio.load(response.data);
-    let artworkUrl = $('meta[name="twitter:image"]').attr('content');
+    const { data } = await axios.get(appleMusicLink);
+    const artworkUrl = cheerio.load(data)('meta[name="twitter:image"]').attr('content');
 
-    if (!artworkUrl) {
-      throw new Error('Artwork URL not found in the page source.');
-    }
+    if (!artworkUrl) throw new Error('Artwork URL not found in the page source.');
 
-    artworkUrl = artworkUrl.replace('/600x600bf-60.jpg', '/1000x1000bf-60.jpg');
-
-    return artworkUrl;
+    return artworkUrl.replace('/600x600bf-60.jpg', '/1000x1000bf-60.jpg');
   } catch (error) {
     throw new Error(`Error: ${error.message}`);
   }
