45e63aefe9342cc9d5226e87816d14df9457d91d
Nansess <75940580+Nansess@users.noreply.github.com>
2023-11-18 14:46:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ async function getArtworkUrl(appleMusicLink) {
   try {
     const response = await axios.get(appleMusicLink);
     const $ = cheerio.load(response.data);
-    const artworkUrl = $('meta[name="twitter:image"]').attr('content');
+    let artworkUrl = $('meta[name="twitter:image"]').attr('content');
 
     if (!artworkUrl) {
       throw new Error('Artwork URL not found in the page source.');
     }
+
+    artworkUrl = artworkUrl.replace('/600x600bf-60.jpg', '/1000x1000bf-60.jpg');
 
     return artworkUrl;
   } catch (error) {
